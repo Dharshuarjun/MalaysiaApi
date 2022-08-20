3863d075eba0553ed80a0fb97899a14e16dbe163
dharshuarjun <dhanalakshm77@gmail.com>
2022-08-20 14:22:12 +0530

    changed

---
+++ script.js
@@ -0,0 +1,33 @@
+var div=document.createElement("div");
+div.innerHTML=`<input type="text"id="txt">
+<button type="button" onclick="foo()">Search</button>
+<div id="popu"></div>
+<div id="state"></div>
+<div id="above18"></div>
+<div id="above60"></div>
+`;
+div.style.textAlign="center";
+div.style.color="red";
+document.body.append(div);
+
+async function foo(){
+    let cc=document.getElementById("txt").value;
+let res=await fetch(`https://myvaccination-backend.vercel.app/api/pop/${cc}`);
+let res1=await res.json();
+console.log(res1.modifiedData.total);
+let result=res1.modifiedData.total;
+document.getElementById("popu").innerText=`Total Population:${result}`;
+
+console.log(res1.modifiedData.stateName);
+let result1=res1.modifiedData.stateName;
+document.getElementById("state").innerText=`StateName:${result1}`;
+
+console.log(res1.modifiedData["18to59"]);
+let result2=res1.modifiedData["18to59"];
+document.getElementById("above18").innerText=`Age 18to 59:${result2}`;
+
+
+console.log(res1.modifiedData["60andAbove"]);
+let result3=res1.modifiedData["60andAbove"];
+document.getElementById("above60").innerText=`60andAbove:${result3}`;
+}
